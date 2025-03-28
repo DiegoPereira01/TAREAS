1872a3f141cbf0ffae53e159265a83e205aa9200
DiegoPereira01 <diegoalejandropereiranegrete@gmail.com>
2025-03-27 20:30:58 -0400

    Add files via upload

---
+++ Tareas/lista dinamica.py
@@ -0,0 +1,7 @@
+#Lista de compras dinámica (lista de compras)
+Lista = {"shampoo":2, "jabon":2, "papel higienico":3, "cepillo":4, "pasta":2,"barra de granola":1,"soda":2,"leche":2,"pan":7}
+Lista["cereal"]=2
+Lista["huevos"]=12
+Lista["queso"]=3
+Lista["jamon"]=2
+print(Lista)
